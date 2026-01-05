612d944499aecd112e530bbe67ea02c2bde57e41
Dipto842 <diptobakshi72@gmail.com>
2026-01-06 02:25:10 +0600

    add whileLoop and for loop

---
+++ PythonVariables/PythonLists/WhileLoopAndforLoop/WhileLoopAndforLoop.py
@@ -0,0 +1,26 @@
+# this time i use a for Loop 
+ThisList= ['sumi','mita','mithila','tuntuni','Love','My Love']
+
+for nam in ThisList:
+    print(nam)
+
+#  use a for loop bat this for loop use a range() and len() 
+
+for i in range(len(ThisList)):
+    print(i)
+
+    # next i use While Loop bat this While Loop rune unlimited 
+
+    # index=0
+
+    # while index < len(ThisList):
+    #    print(ThisList[index])
+
+    # now i use a while loop and this while loop print 1 by 1 litem  or not Unlinited rune 
+    index=0
+    while index < len(ThisList):
+        print(ThisList[index]) 
+        index=index+1
+
+
+
