5e2d0b9b884661323ddcae192bf9640fb1d51d5d
Dipto842 <diptobakshi72@gmail.com>
2026-01-06 00:04:21 +0600

    add Python creat the  List

---
+++ PythonVariables/PythonLists/Lists/Lists.py
@@ -0,0 +1,30 @@
+# Create a List: this is a smail list create
+List = ["apple","Dipto",2,3,True]
+
+print(List)
+# the list is  allow a Duplicates item.  Example : 
+
+TheList = ["Dipto", "Big brther" , "mother", "mother",'father',"Dipto", "Big brther" , "mother", "mother",'father']
+print(TheList)
+
+# List Length :
+TheList = ["Dipto", "Big brther" , "mother", "mother",'father',"Dipto", "Big brther" , "mother", "mother",'father']
+len= len(TheList)
+print(len)
+
+# List item can be of any Data type . example : 
+
+
+List = ["apple","Dipto",2,3,True]
+
+#  type  cheking
+
+List = ["apple","Dipto",2,3,True]
+
+Type = type(List)
+print(Type)
+
+#  The list() Constructor
+ 
+theList = list(("ami","tumi","Love"))
+print(type(theList))
